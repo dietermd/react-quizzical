9fe8a473ab6eef2f994f6ca8130c429221520127
Dieter Mutiz <dieter_md@hotmail.com>
2023-05-25 00:16:54 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <main>
+      <span className="intro-title">Quizzical</span>
+      <span className="intro-description">Test your Knowledge</span>
+      <button className="intro-button">Start quiz</button>
+    </main>
+  )
+}
+
+export default App
