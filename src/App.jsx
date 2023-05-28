1744fd520279c353cce40fadd5fd6298dedf980d
Dieter Mutiz <dieter_md@hotmail.com>
2023-05-27 21:15:01 -0300

    Intro component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react"
+import blob1 from "./assets/blob_1.svg"
+import blob2 from "./assets/blob_2.svg"
+import Intro from "./components/intro"
+
 
 function App() {
 
+  const [gameStart, setGameStart] = useState(false)
+
   return (
     <main>
-      <span className="intro-title">Quizzical</span>
-      <span className="intro-description">Test your Knowledge</span>
-      <button className="intro-button">Start quiz</button>
+      <img src={blob1} className="blob1" alt="blob 1"/>
+      <img src={blob2} className="blob2" alt="blob 2"/>
+      {
+        !gameStart ? 
+        <Intro /> :
+        <h1>Quizz</h1>
+      }
+      
     </main>
   )
 }
---
+++ src/components/intro.jsx
@@ -0,0 +1,10 @@
+
+export default function Intro() {
+  return (
+    <>
+      <span className="intro-title">Quizzical</span>
+      <span className="intro-description">Test your Knowledge</span>
+      <button className="intro-button">Start quiz</button>
+    </>
+  )
+}
